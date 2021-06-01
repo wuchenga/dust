6381517ca98a3266eb2eb0c5a9cd8270b1003257
superbei666 <3260115423@qq.com>
2021-06-01 18:45:29 +0800

    update

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 ## CUSTOM_SHELL_FILE for https://gitee.com/lxk0301/jd_docker/tree/master/docker
-### 编辑docker-compose.yml文件添加: - CUSTOM_SHELL_FILE=https://raw.githubusercontent.com/monk-coder/dust/dust/shell_script_mod.sh
+### 编辑docker-compose.yml文件添加: - CUSTOM_SHELL_FILE=https://raw.githubusercontent.com/wuchenga/dust/dust/shell_script_mod.sh
 #### 容器完全启动后执行 docker exec -it jd_scripts /bin/sh -c 'crontab -l' 查看目前修行的经书
 
-function monkcoder(){
-    # https://github.com/monk-coder/dust
-    rm -rf /monkcoder /scripts/monkcoder_*
-    git clone https://github.com/monk-coder/dust.git /monkcoder
+function wuchenga(){
+    # https://github.com/wuchenga/dust
+    rm -rf /wuchenga /scripts/wuchenga_*
+    git clone https://github.com/wuchenga/dust.git /wuchenga
     # 拷贝脚本
-    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
+    for jsname in $(find /wuchenga -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/wuchenga_${jsname##*/}; done
     # 匹配js脚本中的cron设置定时任务
-    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do
+    for jsname in $(find /wuchenga -name "*.js" | grep -vE "\/backup\/"); do
         jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
-        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/monkcoder_${jsname##*/} >> /scripts/logs/monkcoder_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/wuchenga_${jsname##*/} >> /scripts/logs/wuchenga_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
     done
 }
 
@@ -22,7 +22,7 @@ function main(){
     # DIY脚本执行前后信息
     a_jsnum=$(ls -l /scripts | grep -oE "^-.*js$" | wc -l)
     a_jsname=$(ls -l /scripts | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
-    monkcoder
+    wuchenga
     b_jsnum=$(ls -l /scripts | grep -oE "^-.*js$" | wc -l)
     b_jsname=$(ls -l /scripts | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
     # DIY脚本更新TG通知
